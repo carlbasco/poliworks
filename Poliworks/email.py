9b3cde59553a0bdd025f612636a057c8b72af505
miyabioda <miyabioda@users.noreply.github.com>
2020-06-03 00:44:31 +0800

    fix succession message, fix email bug

--- Poliworks/email.py
+++ Poliworks/email.py
@@ -1,7 +1,8 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rrrsptn@gmail.com'
-EMAIL_HOST_PASSWORD = 'R4spvt1n'
+# EMAIL_HOST_PASSWORD = 'R4spvt1n'
+EMAIL_HOST_PASSWORD = 'jfqumisicteliywv'
 EMAIL_PORT=587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
