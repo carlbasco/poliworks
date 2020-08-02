1d85936717fff274e4a3378e8575b514288b2687
miyabioda <miyabioda@users.noreply.github.com>
2020-08-02 18:46:20 +0800

    moving to alpha state

--- database reset.sql
+++ database reset.sql
@@ -103,3 +103,5 @@ delete from sqlite_sequence where name='`construction_reworkafterimage`';
 
 delete from `construction_externalorderimage`; 
 delete from sqlite_sequence where name='`construction_reworkafterimage`';
+
+UPDATE 'construction_personnel' SET joborder_count=0 WHERE joborder_count=1;
